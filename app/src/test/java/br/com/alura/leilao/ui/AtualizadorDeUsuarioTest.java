dd60e64d122a9703f9da1caa74210e9e2c046c7b
Claudio Costa <claudio@imaginaction.com.br>
2019-10-25 15:47:34 -0300

    feet: 01 - 03 - Simulando retornos de métodos de objetos mockados

--- app/src/test/java/br/com/alura/leilao/ui/AtualizadorDeUsuarioTest.java
+++ app/src/test/java/br/com/alura/leilao/ui/AtualizadorDeUsuarioTest.java
@@ -5,14 +5,14 @@ import android.support.v7.widget.RecyclerView;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import br.com.alura.leilao.database.dao.UsuarioDAO;
 import br.com.alura.leilao.model.Usuario;
 import br.com.alura.leilao.ui.recyclerview.adapter.ListaUsuarioAdapter;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class AtualizadorDeUsuarioTest {
@@ -23,15 +23,20 @@ public class AtualizadorDeUsuarioTest {
     private ListaUsuarioAdapter adapter;
     @Mock
     private RecyclerView recyclerView;
-    private Usuario alex;
 
     @Test
     public void deve_AtualizarListaDeUsuarios_QuandoSalvarUsuarios() {
         final AtualizadorDeUsuario atualizadorDeUsuario = new AtualizadorDeUsuario(dao, adapter, recyclerView);
-        alex = new Usuario("Alexa");
-        atualizadorDeUsuario.salva(alex);
-        verify(dao).salva(alex);
-        verify(adapter).adiciona(alex);
+        Usuario usuarioSemId = new Usuario("Alex");
+        Usuario usuarioComId = new Usuario(1, "Alex");
+
+        when(dao.salva(usuarioSemId)).thenReturn(usuarioComId);
+        when(adapter.getItemCount()).thenReturn(1);
+
+        atualizadorDeUsuario.salva(usuarioSemId);
+
+        verify(dao).salva(usuarioSemId);
+        verify(adapter).adiciona(usuarioComId);
         verify(recyclerView).smoothScrollToPosition(adapter.getItemCount() - 1);
     }
 }
